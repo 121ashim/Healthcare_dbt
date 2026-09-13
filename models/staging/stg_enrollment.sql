0fe89466074f80637896bd0bbb95eb352ee5603b
ashim121 <ashimp355@gmail.com>
2026-09-13 21:59:52 +0545

    Initial healthcare dbt project

---
+++ models/staging/stg_enrollment.sql
@@ -0,0 +1,13 @@
+select
+    NPI,
+    MULTIPLE_NPI_FLAG,
+    PECOS_ASCT_CNTL_ID,
+    ENRLMT_ID,
+    PROVIDER_TYPE_CD,
+    PROVIDER_TYPE_DESC,
+    STATE_CD,
+    FIRST_NAME,
+    MDL_NAME,
+    LAST_NAME,
+    ORG_NAME
+from `healthcare-medicare.raw.enrollment`
